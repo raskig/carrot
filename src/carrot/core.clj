0c9cdee01431a4fdbfecc80ce1e0e61fee3d0ea7
Gabor Raski <gabor.raski@gmail.com>
2017-03-08 12:28:03 +0000

    carrot acknoledges message even if it fails

--- src/carrot/core.clj
+++ src/carrot/core.clj
@@ -53,7 +53,8 @@
                 routing-key
                 message
                 {:persistent true
-                 :headers {"retry-attempts" (inc retry-attempts)}})))
+                 :headers {"retry-attempts" (inc retry-attempts)}}))
+  (lb/ack ch (:delivery-tag meta)))
 
 (defn- message-handler [message-handler routing-key max-retry waiting-exchange dead-letter-exchange logger-fn ch meta ^bytes payload]
   (try
