5f6ed9436c4c19e0b1a70b820d7ef8bcc6f17df9
Gabor Raski <gabor.raski@gmail.com>
2017-03-27 16:33:43 +0100

    assert that the message was added to dead-letter-queue

--- test/carrot/core_test.clj
+++ test/carrot/core_test.clj
@@ -25,9 +25,11 @@
     (let [qname "message-queue"
           latch (java.util.concurrent.CountDownLatch. 1)
           handler-called (atom #{});;we define this atom which will be a global variable where we store the fact that the message has been consumed ok
+          put-in-dead-letter (atom #{});;we define this atom which will be a global variable where we store the fact that the message has been consumed ok
           msg-handler   (fn [{:keys [ch meta payload]}]
                           (.countDown latch));;message handler starts  the countdown when message is arrived: we will read the value of atom when the counter is done.
-          log-called (fn [tag] (fn [_] (swap! handler-called conj tag)))] ;;when this functin is called we swap the atom: we add the caslled tag
+          log-called (fn [tag] (fn [_] (swap! handler-called conj tag)));;when this functin is called we swap the atom: we add the caslled tag
+          dlq-called (fn [tag] (fn [_] (swap! put-in-dead-letter conj tag)))]
       (def carrot-config {:waiting-exchange "waiting-exchange"
                     :dead-letter-exchange "dead-letter-exchange"
                     :waiting-queue "waiting-queue"
@@ -59,13 +61,16 @@
   (with-open [conn (lhc/connect)
               channel (lch/open conn)]
     (let [qname "message-queue"
-          latch (java.util.concurrent.CountDownLatch. 1)
-          handler-called (atom #{});;we define this atom which will be a global variable where we store the fact that the message has been consumed ok
+          latch (java.util.concurrent.CountDownLatch. 1);;counter to be called when message handler first called
+          latch-dead (java.util.concurrent.CountDownLatch. 1);;counter to be called when message handler for the dead letter queue called
+          event-list (atom #{});;we define this atom which will be a global variable where we store the fact that the message has been consumed ok
           msg-handler   (fn [{:keys [ch meta payload]}]
-                          (swap! handler-called conj (keyword (str "retry-" (or (clojure.core/get (:headers meta) "retry-attempts") 0))))
+                          (swap! event-list conj (keyword (str "retry-" (or (clojure.core/get (:headers meta) "retry-attempts") 0))))
                           (.countDown latch)
                           (throw (Exception. "my exception for retry message")));;message handler starts  the countdown when message is arrived: we will read the value of atom when the counter is done.
-          log-called (fn [tag] (fn [_] (swap! handler-called conj tag)))] ;;when this functin is called we swap the atom: we add the caslled tag
+          dead-msg-handler (fn [ch meta payload]
+                          (.countDown latch-dead))
+          log-called (fn [tag] (fn [_] (swap! event-list conj tag)))] ;;when this functin is called we swap the atom: we add the caslled tag
       (def carrot-config {:waiting-exchange "waiting-exchange"
                     :dead-letter-exchange "dead-letter-exchange"
                     :waiting-queue "waiting-queue"
@@ -89,7 +94,9 @@
                          println)
                         {:auto-ack false :handle-consume-ok (log-called :handle-consume-ok)};; consume-ok function is called when message consumption is OK.(thi is in this case the log-called function with a tag.)
                         dead-queue-config-function)
+      (lhcons/subscribe channel "dead-message-queue" dead-msg-handler {:auto-ack true :handle-consume-ok (log-called :handle-dead-message-ok)})
       (lhb/publish channel "message-exchange" qname "dummy payload" { :message-id (str (java.util.UUID/randomUUID))})
       (Thread/sleep 10000)
       (is (.await latch 700 TimeUnit/MILLISECONDS));;await causes the current thread to wait until the latch has counted down to zero, unless the thread is interrupted
-      (is (= #{:handle-consume-ok :retry-0 :retry-1 :retry-2 :retry-3} @handler-called)))))
+      (is (.await latch-dead 700 TimeUnit/MILLISECONDS));;await causes the current thread to wait until the latch has counted down to zero, unless the thread is interrupted
+      (is (= #{:handle-consume-ok :retry-0 :retry-1 :retry-2 :retry-3 :handle-dead-message-ok} @event-list)))))
