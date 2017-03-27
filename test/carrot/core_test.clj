827b028df271841586214ff1ab004f816d70000d
Gabor Raski <gabor.raski@gmail.com>
2017-03-27 15:40:56 +0100

    testcase for retrying

--- test/carrot/core_test.clj
+++ test/carrot/core_test.clj
@@ -19,7 +19,7 @@
 (defn dead-queue-config-function [queue-name]
   {:arguments {"x-max-length" 1000}})
 
-(deftest test-subscribe-with-custom-handler2
+(deftest test-subscribe-with-carrot
   (with-open [conn (lhc/connect)
               channel (lch/open conn)]
     (let [qname "message-queue"
@@ -54,3 +54,42 @@
       (lhb/publish channel "message-exchange" qname "dummy payload" { :message-id (str (java.util.UUID/randomUUID))})
       (is (.await latch 700 TimeUnit/MILLISECONDS));;await causes the current thread to wait until the latch has counted down to zero, unless the thread is interrupted
       (is (= #{:handle-consume-ok} @handler-called)))))
+
+(deftest test-retry-with-carrot
+  (with-open [conn (lhc/connect)
+              channel (lch/open conn)]
+    (let [qname "message-queue"
+          latch (java.util.concurrent.CountDownLatch. 1)
+          handler-called (atom #{});;we define this atom which will be a global variable where we store the fact that the message has been consumed ok
+          msg-handler   (fn [{:keys [ch meta payload]}]
+                          (swap! handler-called conj (keyword (str "retry-" (or (clojure.core/get (:headers meta) "retry-attempts") 0))))
+                          (.countDown latch)
+                          (throw (Exception. "my exception for retry message")));;message handler starts  the countdown when message is arrived: we will read the value of atom when the counter is done.
+          log-called (fn [tag] (fn [_] (swap! handler-called conj tag)))] ;;when this functin is called we swap the atom: we add the caslled tag
+      (def carrot-config {:waiting-exchange "waiting-exchange"
+                    :dead-letter-exchange "dead-letter-exchange"
+                    :waiting-queue "waiting-queue"
+                          :message-exchange "message-exchange"})
+      (carrot/declare-system channel
+                           carrot-config
+                           3000
+                           "topic"
+                           {:durable true}
+                           {:arguments {"x-max-length" 1000}})
+      (lhq/declare channel qname {:exclusive false :auto-delete false})
+      (lhq/bind channel qname "message-exchange" {:routing-key qname})
+      (carrot/subscribe channel
+                        carrot-config
+                        qname
+                        (carrot/crate-message-handler-function
+                         msg-handler
+                         qname
+                         3
+                         carrot-config
+                         println)
+                        {:auto-ack false :handle-consume-ok (log-called :handle-consume-ok)};; consume-ok function is called when message consumption is OK.(thi is in this case the log-called function with a tag.)
+                        dead-queue-config-function)
+      (lhb/publish channel "message-exchange" qname "dummy payload" { :message-id (str (java.util.UUID/randomUUID))})
+      (Thread/sleep 10000)
+      (is (.await latch 700 TimeUnit/MILLISECONDS));;await causes the current thread to wait until the latch has counted down to zero, unless the thread is interrupted
+      (is (= #{:handle-consume-ok :retry-0 :retry-1 :retry-2 :retry-3} @handler-called)))))
