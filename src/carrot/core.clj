d4eb2dd6bffe613da7d42dbc1c59bceee70529a3
Gabor Raski <gabor.raski@gmail.com>
2017-03-28 14:45:50 +0100

    implement exponencial backoff

--- src/carrot/core.clj
+++ src/carrot/core.clj
@@ -43,9 +43,14 @@
                   (assoc meta :reject? true ))))
 
 
-(defn- nack [ch message meta routing-key retry-attempts max-retry waiting-exchange dead-letter-exchange logger-fn]
+(defn- nack [ch message meta routing-key retry-attempts retry-config waiting-exchange dead-letter-exchange logger-fn]
   (let [retry-attempts (int retry-attempts)
-        exchange (if (> max-retry retry-attempts) waiting-exchange dead-letter-exchange)]
+        exponential-backoff? (:max-retry-count retry-config)
+        max-retry (if exponential-backoff? (:max-retry-count retry-config) retry-config)
+        exchange (if (> max-retry retry-attempts) waiting-exchange dead-letter-exchange)
+        exp-backoff-config (if exponential-backoff?
+                             {:expiration (str (.pow (BigInteger. (str (:initial-ttl retry-config))) (int (+ 1 retry-attempts))))}
+                             {})]
     (when logger-fn (logger-fn "LOGME ns=carrot.core name=current-retry-attempt message:" (:message-id meta) retry-attempts))
     (when logger-fn (logger-fn "Sending message " (:message-id meta)  " to the exchange: " exchange))
     (lb/publish ch
@@ -54,11 +59,12 @@
                 message
                 (merge
                  meta
+                 exp-backoff-config
                  {:persistent true
                   :headers {"retry-attempts" (inc retry-attempts)}})))
   (lb/ack ch (:delivery-tag meta)))
 
-(defn- message-handler [message-handler routing-key max-retry waiting-exchange dead-letter-exchange logger-fn ch meta ^bytes payload]
+(defn- message-handler [message-handler routing-key retry-config waiting-exchange dead-letter-exchange logger-fn ch meta ^bytes payload]
   (try
     (let [carrot-map {:channel ch
                       :meta meta
@@ -71,7 +77,7 @@
       (when logger-fn
         (logger-fn "LOGME ns=carrot.core name=message-process-error message-id="(:message-id meta) "error="e " exception="(clojure.stacktrace/print-stack-trace e)))
       (if (retry-exception? e logger-fn)
-        (nack ch payload meta routing-key (or (get (:headers meta) "retry-attempts") 0) max-retry waiting-exchange dead-letter-exchange logger-fn)
+        (nack ch payload meta routing-key (or (get (:headers meta) "retry-attempts") 0) retry-config waiting-exchange dead-letter-exchange logger-fn)
         (lb/ack ch (:delivery-tag meta))))))
 
 
@@ -82,16 +88,20 @@
     exchange-type
     exchange-config
     waiting-queue-config]
+   (def my_message-ttl message-ttl)
    (le/declare channel waiting-exchange exchange-type exchange-config)
    (le/declare channel message-exchange exchange-type exchange-config)
    (le/declare channel dead-letter-exchange exchange-type exchange-config)
-   (let [waiting-queue-name (:queue (lq/declare channel waiting-queue
-                                                                   (merge-with merge waiting-queue-config
-                                                                                 {:exlusive false
-                                                                                  :auto-delete true
-                                                                                  :durable true
-                                                                                  :arguments {"x-message-ttl" message-ttl
-                                                                                              "x-dead-letter-exchange" message-exchange}})))]
+   (let [message-ttl-config (if (not  (or (= message-ttl 0) (= message-ttl "N/A")))  {:arguments {"x-message-ttl" message-ttl}} {})
+         waiting-queue-name (:queue (lq/declare channel waiting-queue
+                                                (merge-with merge
+                                                            waiting-queue-config
+                                                            message-ttl-config
+                                                            {:exlusive false
+                                                             :auto-delete true
+                                                             :durable true
+                                                             :arguments {
+                                                                         "x-dead-letter-exchange" message-exchange}})))]
      (lq/bind channel waiting-queue-name waiting-exchange {:routing-key "#"})))
   ([channel
     carrot-system
@@ -105,6 +115,14 @@
                    exchange-config
                    {})))
 
+(defn destroy-system [channel {:keys [waiting-exchange dead-letter-exchange waiting-queue message-exchange]} queue-name-coll]
+  (map #(lq/delete channel %) queue-name-coll)
+  (map #(lq/delete channel (str "dead-" %)) queue-name-coll)
+  (lq/delete channel waiting-queue)
+  (le/delete channel waiting-exchange)
+  (le/delete channel dead-letter-exchange)
+  (le/delete channel message-exchange))
+
 
 (defn subscribe
   ([channel
@@ -116,7 +134,8 @@
    (lc/subscribe channel queue-name message-handler queue-config)
    (lq/declare channel (str "dead-" queue-name)
                (merge-with merge (dead-queue-config-function queue-name)
-                      {:exclusive false :auto-delete false}))
+                           {:exclusive false
+                            :auto-delete false}))
    (lq/bind channel (str "dead-" queue-name) dead-letter-exchange {:routing-key queue-name}))
   ([channel
     carrot-config
@@ -131,10 +150,10 @@
               {})))
 
 (defn crate-message-handler-function
-  ([handler routing-key max-retry {:keys [waiting-exchange dead-letter-exchange]} logger-fn]
-   (partial message-handler handler routing-key max-retry waiting-exchange dead-letter-exchange logger-fn))
-  ([handler routing-key max-retry {:keys [waiting-exchange dead-letter-exchange]}]
-   (crate-message-handler-function message-handler handler routing-key max-retry waiting-exchange dead-letter-exchange nil)))
+  ([handler routing-key retry-config {:keys [waiting-exchange dead-letter-exchange]} logger-fn]
+   (partial message-handler handler routing-key retry-config waiting-exchange dead-letter-exchange logger-fn))
+  ([handler routing-key retry-config {:keys [waiting-exchange dead-letter-exchange]}]
+   (crate-message-handler-function message-handler handler routing-key retry-config waiting-exchange dead-letter-exchange nil)))
 
 (defmacro compose-payload-handler-function
   [& args]
--- test/carrot/core_test.clj
+++ test/carrot/core_test.clj
@@ -16,6 +16,22 @@
            java.util.concurrent.TimeUnit))
 
 
+(def carrot-config {:waiting-exchange "waiting-exchange"
+                    :dead-letter-exchange "dead-letter-exchange"
+                    :waiting-queue "waiting-queue"
+                          :message-exchange "message-exchange"})
+
+(deftest destroy
+  (with-open [conn (lhc/connect)
+              channel (lch/open conn)]
+    (carrot/destroy-system channel carrot-config '("message-queue"))))
+
+(defn with-destroy [f]
+  (f)
+  (destroy))
+
+(use-fixtures :each with-destroy)
+
 (defn dead-queue-config-function [queue-name]
   {:arguments {"x-max-length" 1000}})
 
@@ -30,17 +46,13 @@
                           (.countDown latch));;message handler starts  the countdown when message is arrived: we will read the value of atom when the counter is done.
           log-called (fn [tag] (fn [_] (swap! handler-called conj tag)));;when this functin is called we swap the atom: we add the caslled tag
           dlq-called (fn [tag] (fn [_] (swap! put-in-dead-letter conj tag)))]
-      (def carrot-config {:waiting-exchange "waiting-exchange"
-                    :dead-letter-exchange "dead-letter-exchange"
-                    :waiting-queue "waiting-queue"
-                          :message-exchange "message-exchange"})
       (carrot/declare-system channel
                            carrot-config
                            3000
                            "topic"
                            {:durable true}
                            {:arguments {"x-max-length" 1000}})
-      (lhq/declare channel qname {:exclusive false :auto-delete false})
+      (lhq/declare channel qname {:exclusive false :auto-delete true})
       (lhq/bind channel qname "message-exchange" {:routing-key qname})
       (carrot/subscribe channel
                         carrot-config
@@ -56,6 +68,12 @@
       (lhb/publish channel "message-exchange" qname "dummy payload" { :message-id (str (java.util.UUID/randomUUID))})
       (is (.await latch 700 TimeUnit/MILLISECONDS));;await causes the current thread to wait until the latch has counted down to zero, unless the thread is interrupted
       (is (= #{:handle-consume-ok} @handler-called)))))
+
+
+
+
+
+
 
 (deftest test-retry-with-carrot
   (with-open [conn (lhc/connect)
@@ -81,7 +99,7 @@
                            "topic"
                            {:durable true}
                            {:arguments {"x-max-length" 1000}})
-      (lhq/declare channel qname {:exclusive false :auto-delete false})
+      (lhq/declare channel qname {:exclusive false :auto-delete true})
       (lhq/bind channel qname "message-exchange" {:routing-key qname})
       (carrot/subscribe channel
                         carrot-config
@@ -97,6 +115,56 @@
       (lhcons/subscribe channel "dead-message-queue" dead-msg-handler {:auto-ack true :handle-consume-ok (log-called :handle-dead-message-ok)})
       (lhb/publish channel "message-exchange" qname "dummy payload" { :message-id (str (java.util.UUID/randomUUID))})
       (Thread/sleep 10000)
+      (is (.await latch 700 TimeUnit/MILLISECONDS));;await causes the current thread to wait until the latch has counted down to zero, unless the thread is interrupted
+      (is (.await latch-dead 700 TimeUnit/MILLISECONDS));;await causes the current thread to wait until the latch has counted down to zero, unless the thread is interrupted
+      (is (= #{:handle-consume-ok :retry-0 :retry-1 :retry-2 :retry-3 :handle-dead-message-ok} @event-list)))))
+
+
+
+
+
+
+
+(deftest test-expo-retry-with-carrot
+  (with-open [conn (lhc/connect)
+              channel (lch/open conn)]
+    (let [qname "message-queue"
+          latch (java.util.concurrent.CountDownLatch. 1);;counter to be called when message handler first called
+          latch-dead (java.util.concurrent.CountDownLatch. 1);;counter to be called when message handler for the dead letter queue called
+          event-list (atom #{});;we define this atom which will be a global variable where we store the fact that the message has been consumed ok
+          msg-handler   (fn [{:keys [ch meta payload]}]
+                          (swap! event-list conj (keyword (str "retry-" (or (clojure.core/get (:headers meta) "retry-attempts") 0))))
+                          (.countDown latch)
+                          (throw (Exception. "my exception for retry message")));;message handler starts  the countdown when message is arrived: we will read the value of atom when the counter is done.
+          dead-msg-handler (fn [ch meta payload]
+                          (.countDown latch-dead))
+          log-called (fn [tag] (fn [_] (swap! event-list conj tag)))] ;;when this functin is called we swap the atom: we add the caslled tag
+      (def carrot-config {:waiting-exchange "waiting-exchange"
+                    :dead-letter-exchange "dead-letter-exchange"
+                    :waiting-queue "waiting-queue"
+                          :message-exchange "message-exchange"})
+      (carrot/declare-system channel
+                           carrot-config
+                           0;;no ttl set!!!
+                           "topic"
+                           {:durable true}
+                           {:arguments {"x-max-length" 1000}})
+      (lhq/declare channel qname {:exclusive false :auto-delete true})
+      (lhq/bind channel qname "message-exchange" {:routing-key qname})
+      (carrot/subscribe channel
+                        carrot-config
+                        qname
+                        (carrot/crate-message-handler-function
+                         msg-handler
+                         qname
+                         {:initial-ttl 30 :max-retry-count 3}
+                         carrot-config
+                         println)
+                        {:yauto-ack false :handle-consume-ok (log-called :handle-consume-ok)};; consume-ok function is called when message consumption is OK.(thi is in this case the log-called function with a tag.)
+                        dead-queue-config-function)
+      (lhcons/subscribe channel "dead-message-queue" dead-msg-handler {:auto-ack true :handle-consume-ok (log-called :handle-dead-message-ok)})
+      (lhb/publish channel "message-exchange" qname "dummy payload" { :message-id (str (java.util.UUID/randomUUID))})
+      (Thread/sleep (* 2 (+ 30 (* 30 30) (* 30 30 30))))
       (is (.await latch 700 TimeUnit/MILLISECONDS));;await causes the current thread to wait until the latch has counted down to zero, unless the thread is interrupted
       (is (.await latch-dead 700 TimeUnit/MILLISECONDS));;await causes the current thread to wait until the latch has counted down to zero, unless the thread is interrupted
       (is (= #{:handle-consume-ok :retry-0 :retry-1 :retry-2 :retry-3 :handle-dead-message-ok} @event-list)))))
