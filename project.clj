0c3de1e4f465a4675a13dad79f0f64df142b7cbc
Gabor Raski <gabor.raski@gmail.com>
2017-04-11 10:24:47 +0100

    terminology change: dead letter queues get .dead suffix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject carrot "1.0.1"
+(defproject carrot "2.0.0"
   :description "A Clojure library designed to providing the implementation of  RabbitMq delayed retry mechanism."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
